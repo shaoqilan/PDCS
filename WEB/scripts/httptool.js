fc6fff90f13cd274907fa792b8694ec167af64a4
少奇 <shaoqilan@live.com>
2017-09-29 22:53:21 +0800

    no message

--- WEB/scripts/httptool.js
+++ WEB/scripts/httptool.js
@@ -1,4 +1,10 @@
 ﻿var LOGINUSERSESSION = { "ROLE_ID": "123123", "USER_ID": "heyi001" };//测试用
+//设置登录的用户信息
+function APPSetLoginUser(ROLE_ID, USER_ID) {
+    LOGINUSERSESSION.ROLE_ID = ROLE_ID;
+    LOGINUSERSESSION.USER_ID = USER_ID;
+    alert("用户登录信息设置成功" + USER_ID);
+}
 (function ($) {
     $.extend({
         HttpPost: function (url, reqJson, type, success) {
@@ -11,17 +17,36 @@
                 type: type,
                 success: function (ret) {
                     if (success) {
-                        if (ret) {
-                            success({ ec: 0, em: "数据返回错误" });
-                        }
-                        try {
-                            success(eval("(" + $.base64.decode(ret, true) + ")"));
-                        } catch (e) {
-                            success({ec:0,em:"数据返回错误"});
-                        }
+                        success(eval("(" + $.base64.decode(ret, true) + ")"));
                     }
                 }
             });
         },
+        ShowLoad: function (txt) {
+            //显示load加载动画
+            alert("调用APP 显示Load动画");
+            try {
+                if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+                    aPPIOS.showLoad(txt);
+                } else if (/(Android)/i.test(navigator.userAgent)) {
+                    aPPAndroid.showLoad(txt);
+                }
+            } catch (e) {
+
+            }
+        },
+        HideLoad: function () {
+            //隐藏动画
+            alert("调用APP 隐藏Load动画");
+            try {
+                if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+                    aPPIOS.hideLoad();
+                } else if (/(Android)/i.test(navigator.userAgent)) {
+                    aPPAndroid.hideLoad();
+                }
+            } catch (e) {
+
+            }
+        }
     });
 }(jQuery));
